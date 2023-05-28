d23eb93fa575d5e2447f7cb1963d7fcef929ecdc
MOON <wenhaichen1992@gmail.com>
2023-05-28 17:02:18 +0900

    add entity of core data and an enum class

---
+++ ExpressPackageTracker/ExpressCompany.swift
@@ -0,0 +1,14 @@
+//
+//  ExpressCompany.swift
+//  ExpressPackageTracker
+//
+//  Created by MOON on 2023/05/28.
+//
+
+import Foundation
+
+enum ExpressCompany:String, CaseIterable {
+    case PostJapan = "日本郵便"
+    case Yamato = "ヤマト運輸"
+    case Sagawa = "佐川急便"
+}
